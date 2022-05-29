43f606f62a11791b718adc7ae4c6462f55a40177
visotnik <tet129@gmail.com>
2022-05-29 18:18:28 +0300

    fix merge features/user-access in master

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 console.log("Hello world!");
 
-<<<<<<< HEAD
 const userAge = 35;
 if (userAge >= 18) {
-=======
-const age = 20;
-if (age >= 18) {
->>>>>>> features/user-access
   console.log("You are an adult.");
 } else {
   console.log("You are not an adult.");
